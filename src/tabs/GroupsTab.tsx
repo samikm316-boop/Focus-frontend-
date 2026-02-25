f3f000ccee40ca20ddf576a1049a430f919f03a8
samikm316-boop <samikm316@gmail.com>
2026-02-25 16:44:07 +0530

    Create GroupsTab.tsx

---
+++ src/tabs/GroupsTab.tsx
@@ -0,0 +1,22 @@
+export default function GroupsTab() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Groups & Chats</h2>
+
+      <p>
+        This will contain:
+      </p>
+
+      <ul>
+        <li>Left: Group list</li>
+        <li>Top-right: New Group button</li>
+        <li>AI Chat section</li>
+        <li>Previous AI chat history</li>
+      </ul>
+
+      <p>
+        Full implementation will be added next phase.
+      </p>
+    </div>
+  );
+}
